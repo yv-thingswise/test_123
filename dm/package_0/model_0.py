9c963bec9d27a6cf7bcda9a9918729e1f8dce9f6
yv-thingswise <yv-thingswise>
2023-02-16 10:34:36 +0000

    Model Designer: (auto) commit generated configuration

--- dm/package_0/model_0.py
+++ dm/package_0/model_0.py
@@ -2,6 +2,7 @@ from digital_machine import *
 from digital_machine.templates import simple
 from digital_machine.templates import templates as t
 
+
 class model_0(simple.SimpleClassTemplate):
   
   pass
--- src/dt/package_0/dm/task0.py
+++
@@ -1,6 +0,0 @@
-from digital_machine.batch import BatchContext
-
-def run():
-    B = BatchContext.get()
-
-    # Task code goes here...
